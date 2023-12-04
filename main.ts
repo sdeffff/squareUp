66345a90ee7724f251b825e5b3c1d0c07081fbf9
Maksym <41970421+sdeffff@users.noreply.github.com>
2023-12-04 03:34:43 +0200

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
-//npx tsc --init = tsconfig
-//npm init -y = package json
-//npm i --save-dev typescript = download ts
+//#1 npm i --save-dev typescript = download ts
+//#2 npm init -y = package json
+//#2.1 in package json in scripts: "start": "tsc --watch";
 
-//in package json in scripts: "start": "tsc --watch";
+//#3 npx tsc --init = tsconfig
 
-// npm install -D tailwindcss
-// npx tailwindcss init
+//-----------------------------------------------------------------------------------------
 
-//npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch
+//#1 npm install -D tailwindcss
+//#2 npx tailwindcss init
+
+//#3 npx tailwindcss -i ./src/css/input.css -o ./src/css/output.css --watch
+
+//-----------------------------------------------------------------------------------------
+ 
--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,4 @@
 // npm install -D tailwindcss
 // npx tailwindcss init
 //npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
+ 
